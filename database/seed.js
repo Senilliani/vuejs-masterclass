3863c686627e87adeb4e6d265d0dedb8acd6beb3
Senilliani <bren.senilliani@gmail.com>
2025-10-12 12:06:41 +0200

    fill with data

--- database/seed.js
+++ database/seed.js
@@ -6,15 +6,20 @@ import { createClient } from '@supabase/supabase-js'
 
 const mysupabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-const seedProjects = async () => {
-  const name = faker.lorem.words(2)
+const seedProjects = async (numEntries) => {
+  const projects = []
 
-  await mysupabase.from('projects').insert({
-    name: name,
-    slug: name.toLowerCase().replace(/ /g, '-'),
-    status: faker.helpers.arrayElement(['in-progress', 'completed']),
-    collaborators: faker.helpers.arrayElements([1, 2, 3]),
-  })
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(2)
+
+    projects.push({
+      name: name,
+      slug: name.toLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+    })
+  }
+  await mysupabase.from('projects').insert(projects)
 }
 
-await seedProjects()
+await seedProjects(10)
