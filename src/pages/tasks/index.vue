0b7c6288d3f054ba28fd1c6a21c89d4e816e7959
Senilliani <bren.senilliani@gmail.com>
2025-10-26 18:17:01 +0100

    use tanstack and create data table component

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,20 +1,13 @@
 <template>
-  <div>
-    <h2>Páginas de TAREAS</h2>
-    <RouterLink to="/">Ir a la Home</RouterLink>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">
-        {{ task.name }}
-      </li>
-    </ul>
-  </div>
+  <DataTable :columns="columns" :data="payments" />
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
 import { mysupabase } from '@/lib/supabaseClient';
-import { ref } from 'vue';
+import { h, ref } from 'vue';
 import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table';
+import DataTable from '@/components/ui/data-table/DataTable.vue';
 
 const tasks = ref<Tables<'tasks'>[] | null>();
 
@@ -31,6 +24,43 @@ const f = async () => {
 
 f()
 
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  }
+]
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  }
+]
 </script>
 
 <style scoped></style>
