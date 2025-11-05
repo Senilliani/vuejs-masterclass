be0fa2cd624e8cccfce43257ffaedbd21a869e80
Senilliani <bren.senilliani@gmail.com>
2025-11-05 12:38:10 +0100

    fix use flexrender instead of slot

--- src/components/ui/data-table/DataTable.vue
+++ src/components/ui/data-table/DataTable.vue
@@ -43,10 +43,7 @@ const table = useVueTable({
           <TableRow v-for="row in table.getRowModel().rows" :key="row.id"
             :data-state="row.getIsSelected() ? 'selected' : undefined">
             <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-              <slot :name="`cell-${cell.column.id}`" :cell="cell">
-                {{ cell.getValue() }}
-              </slot>
-              <!-- <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" /> -->
+              <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
             </TableCell>
           </TableRow>
         </template>
